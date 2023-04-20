b2728a13c6421d7e6396a4b7ede7f6dc68de8378
PryceA <129968506+PryceA@users.noreply.github.com>
2023-04-19 20:02:48 -0600

    Update UI and split out main.js

---
+++ webapp/frontend/main.js
@@ -0,0 +1,60 @@
+const skillIds = {
+    A: "Cooperation and Reinforcer Effectiveness",
+    B: "Visual Performance",
+    C: "Receptive Language"
+};
+
+function fetchData(studentId, skillLetter) {
+    return $.get(`/student/${studentId}/skill/${skillLetter}`);
+}
+
+function createGraphCard(skillLetter, skillName, scores) {
+    const graphId = `graph-${skillLetter}`;
+    const card = $(`<div class="card"><div id="${graphId}"></div></div>`);
+
+    const trace = {
+        x: scores.map((_, i) => i + 1),
+        y: scores,
+        mode: 'lines',
+        name: skillName
+    };
+
+    const layout = {
+        title: skillName,
+        xaxis: {title: 'Data Points'},
+        yaxis: {title: 'Score'}
+    };
+
+    Plotly.newPlot(graphId, [trace], layout);
+    return card;
+}
+
+function updateWelcomeMessage(studentId) {
+    const welcomeMessage = $(`#welcome-message`);
+    welcomeMessage.text(`Welcome, Student ${studentId}`);
+}
+
+function updateGraphs(studentId) {
+    const cardsContainer = $('#cards-container');
+    cardsContainer.empty();
+    for (const [skillLetter, skillName] of Object.entries(skillIds)) {
+        fetchData(studentId, skillLetter).then(scores => {
+            const card = createGraphCard(skillLetter, skillName, scores);
+            cardsContainer.append(card);
+        });
+    }
+}
+
+$(document).ready(function () {
+    const studentIdInput = $('#student-id-input');
+    const fetchDataButton = $('#fetch-data-button');
+    let studentId = studentIdInput.val();
+    updateWelcomeMessage(studentId);
+    updateGraphs(studentId);
+
+    fetchDataButton.on('click', function () {
+        studentId = studentIdInput.val();
+        updateWelcomeMessage(studentId);
+        updateGraphs(studentId);
+    });
+});
